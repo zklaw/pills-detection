bd04900ba714ad0fbc7db3a8f484aee75742f9a0
zklaw <zklawikowska97@gmail.com>
2023-10-17 17:54:36 +0200

    added dataloaders to train.py

--- functions.py
+++ functions.py
@@ -1,17 +1,74 @@
 import albumentations as A
+from dataset import PillsDataset
+import torch
+import math
+import numpy as np
+from torch.utils.data import Subset
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+img_width = 224
+img_height = 224
+channels = 3
 
 def get_transform(train):
   if train:
-    transform = A.Compose([A.Resize(224,224,3),
+    transform = A.Compose([A.Resize(img_height, img_width, channels),
                            A.HorizontalFlip(p=0.5),
                            A.RandomBrightnessContrast(p=0.2),
                           #  A.Normalize(mean=(137, 143, 134), std=(50, 52, 48), max_pixel_value=255.0),
                           #  ToTensorV2(),
                            ],bbox_params=A.BboxParams(format='pascal_voc', label_fields = []))
   else:
-    transform = A.Compose([A.Resize(224,224,3),
+    transform = A.Compose([A.Resize(img_height, img_width, channels),
                           #  ToTensorV2(),
                           #  A.Normalize(mean=(137, 143, 134), std=(50, 52, 48), max_pixel_value=255.0),
                            ],
                            bbox_params=A.BboxParams(format='pascal_voc', label_fields = []))
   return transform
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+def split_data(path,device, train_perc, val_perc, test_perc):
+
+    trainset = PillsDataset(path, get_transform(True), device)
+    valset = PillsDataset(path, get_transform(False), device)
+
+    indices = torch.randperm(len(trainset)).tolist()
+
+    a = len(trainset)-math.floor(len(trainset)*train_perc)
+    b = math.floor(len(trainset)*val_perc)
+
+    trainset = Subset(trainset, indices[:-a])
+    valset = Subset(valset, indices[-a:])
+
+    indices = torch.randperm(len(valset)).tolist()
+
+    testset = Subset(valset, indices[:-b])
+    valset = Subset(valset, indices[-b:])
+
+    return trainset, valset, testset
+
+def plot_batch(dataloader):
+
+    x, y = next(iter(dataloader))
+    batch_size = len(x)
+
+    for i in range(batch_size):
+        img = x[i].cpu().detach().numpy().astype(np.int32)
+        ann = y[i]
+        ann = ann['boxes'].cpu().detach().numpy()
+
+        plt.imshow(img)
+        ax = plt.gca()
+
+        for bbox in ann:
+            a = bbox[0]
+            b = bbox[1]
+            width = bbox[2]-bbox[0]
+            height = bbox[3]-bbox[1]
+            rect = Rectangle((a, b),width,height,linewidth=1,edgecolor='r',facecolor='none')
+            ax.add_patch(rect)
+
+        plt.show()
--- train.py
+++ train.py
@@ -1,13 +1,20 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
-
+import math
 from dataset import PillsDataset
-from functions import get_transform
+from functions import get_transform, collate_fn, split_data, plot_batch
+
+batch_size = 10
+idx = 30
+a= 0.6
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-dataset = PillsDataset('pillsdetectiondataset/pillsPicture', get_transform(False), device)
-img, ann = dataset.__getitem__(0)
-plt.imshow(img.cpu().detach().numpy().astype(np.int32))
-plt.show()
+trainset, valset, testset = split_data('pillsdetectiondataset\pillsPicture', device, train_perc=0.8, val_perc=0.15, test_perc=0.05)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+
+plot_batch(trainloader)
